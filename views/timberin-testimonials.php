70df97e6ee4d503a5e8861e9b9b448f824d89552
Paul <paulius.kimbartas@gmail.com>
2017-10-18 20:13:59 +0300

    added additional style for the list thumbnail not to be squished  when picture is not square

--- views/timberin-testimonials.php
+++ views/timberin-testimonials.php
@@ -2,7 +2,7 @@
 <div class="testimonial two-fourth <?php echo $i++ % 2 ? '' : 'last' ?>">
     <div class="thumbnail">
         <a class="name" href="<?php echo $testimonial['url'] ?>">
-            <img title="" width="94" height="94" src="<?php echo $testimonial['thumbnail'] ?>" class="">
+            <img title="" width="94" height="94" src="<?php echo $testimonial['thumbnail'] ?>" class="" style="object-fit: cover;">
         </a>
     </div>
     <div class="testimonial-text">
